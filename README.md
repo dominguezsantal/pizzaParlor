63fb89324cb04d8e77ee4782e2727b3d6cf363a1
Alex Dominguez <alexdominguezwebdev@gmail.com>
2021-07-17 20:55:29 -0700

    pizza form constructor

---
+++ README.md
@@ -0,0 +1,5 @@
+Test 1
+Describe: Add a constructor for pizza form
+Code : function Pizza(parameter1, parameter2,parameter3,parameter4){ }
+_Test: 
+; _Expected Output: The constructor will be the blueprint for the prototype of the object parameter and the proto object of this form.
---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+//Business Logic
+//constructor or object's blueprint of pizza form
+function Pizza(size, meat, cheese, toppings, sauce) {
+    this.size = size;
+    this.meat = meat;
+    this.cheese = cheese;
+    this.toppings = toppings;
+    this.sauce = sauce;
+}
+
+
+
+
+
+
+
+//User Interface
