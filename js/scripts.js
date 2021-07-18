e85ac9942c74aa85ae1f5ec423039dbc2c69250a
Alex Dominguez <alexdominguezwebdev@gmail.com>
2021-07-18 06:28:41 -0700

    debbuging of size + meat result

--- js/scripts.js
+++ js/scripts.js
@@ -13,69 +13,70 @@ function Pizza(size, meat, cheese, toppings, sauce) {
 
 //prototype for pizza size cost
 Pizza.prototype.calculateSizeCost = function() {
-    if (this.size === "small") {
-        this.cost = 6;
-    } else if (this.size === "medium") {
-        this.cost = 8;
-    } else if (this.size === "large") {
-        this.cost = 10;
-    } else if (this.size === "extra-large") {
-        this.cost = 12;
+        if (this.size === "small") {
+            this.cost = 6;
+        } else if (this.size === "medium") {
+            this.cost = 8;
+        } else if (this.size === "large") {
+            this.cost = 10;
+        } else if (this.size === "extra-large") {
+            this.cost = 12;
+        }
+        return this.cost;
+
     }
-    return this.cost;
-
-}
-
-
-//meat cost
-//Pizza.prototype.calculateMeat = function() {
-//        if (this.meat === "chicken") {
-//            this.cost = this.cost + 2;
-//        } else if (this.meat === "bacon") {
-//            this.cost = this.cost + 2;
-//        } else if (this.size === "pepperoni") {
-//            this.cost = this.cost + 2;
-//        } else if (this.size === "salmon") {
-//            this.cost = this.cost + 4;
-//        } else if (this.size === "steak") {
-//            this.cost = this.cost + 5;
-//        }
-//    }
-//    //cheese cost
-//Pizza.prototype.calculateCheese = function() {
-//    if (this.cheese === "mozarella") {
-//        this.cost = this.cost + 1;
-//    } else if (this.cheese === "harvati") {
-//        this.cost = this.cost + 1;
-//    } else if (this.size === "provolone") {
-//        this.cost = this.cost + 1;
-//    } else if (this.size === "american") {
-//        this.cost = this.cost + 4;
-//    }
-//}//
-//
-//
-//
-//
-//
-//
-//
+    //meat cost
+Pizza.prototype.calculateMeatCost = function() {
+        if (this.meat === "chicken") {
+            this.cost = 2;
+        } else if (this.meat === "bacon") {
+            this.cost = 2;
+        } else if (this.size === "pepperoni") {
+            this.cost = 2;
+        } else if (this.size === "salmon") {
+            this.cost = 4;
+        } else if (this.size === "steak") {
+            this.cost = 5;
+        }
+        return this.cost;
+    }
+    //    //cheese cost
+    //Pizza.prototype.calculateCheese = function() {
+    //    if (this.cheese === "mozarella") {
+    //        this.cost = this.cost + 1;
+    //    } else if (this.cheese === "harvati") {
+    //        this.cost = this.cost + 1;
+    //    } else if (this.size === "provolone") {
+    //        this.cost = this.cost + 1;
+    //    } else if (this.size === "american") {
+    //        this.cost = this.cost + 4;
+    //    }
+    //}//
+    //
+    //
+    //
+    //
+    //
+    //
+    //
 
 //User Interface
 $(document).ready(function() {
     $("#pizza-form").submit(function(event) {
         event.preventDefault();
         let size = $("input:radio[name='size']:checked").val();
-        // let meat = $("input:checkbox[name='meat']:checked").val();
+        let meat = $("input:checkbox[name='meat']:checked").val();
         // let cheese = $("input:checkbox[name='cheese']:checked").val();
 
 
-        let selectedPizza = new Pizza(size);
+        //obj for customer's pizza choice
+        let selectedPizza = new Pizza(size, meat);
 
         //final cost of pizza(s)
-        selectedPizza.calculateSizeCost();
+        let showCost = selectedPizza.calculateSizeCost() + selectedPizza.calculateMeatCost();
 
-        let showCost = selectedPizza.cost;
+
+        //let showCost = selectedPizza.cost;
 
 
         $("#finalCost").show();
