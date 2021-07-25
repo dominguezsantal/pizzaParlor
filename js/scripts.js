4503048007093acc5df2316c37a8b5529e5928cc
Alex Dominguez <alexdominguezwebdev@gmail.com>
2021-07-24 21:21:20 -0700

    add text method on line 76 of scripts. It will display in text result

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ function Pizza(size, meat, cheese, toppings) {
 }
 
 //prototype for pizza size cost
-Pizza.prototype.calculateSizeCost = function() {
+Pizza.prototype.calculatePizzaCost = function() {
     if (this.size === "small") {
         this.cost = this.cost += 6;
     } else if (this.size === "medium") {
@@ -19,65 +19,62 @@ Pizza.prototype.calculateSizeCost = function() {
     } else if (this.size === "extra-large") {
         this.cost = this.cost += 12;
     }
+    //meat cost
+    for (var i = 0; i < this.meat.length; i++) {
+        this.cost += 4;
+    }
+    //cheese cost
+    for (var i = 0; i < this.cheese.length; i++) {
+        this.cost += 1;
+    }
+    //toppings cost
+    for (var i = 0; i < this.toppings.length; i++) {
+        this.cost += 1;
+    }
 }
 
-//meat cost
-for (var i = 0; i < this.meat.length; i++) {
-    this.cost += 4;
-}
-//cheese cost
-for (var i = 0; i < this.cheese.length; i++) {
-    this.cost += 1;
-}
-
-//toppings cost
-for (var i = 0; i < this.toppings.length; i++) {
-    this.cost += 1;
-
-
-    //User Interface
-    $(document).ready(function() {
-        $("form#pizza-form").submit(function(event) {
-            event.preventDefault();
-            let size = $("input:radio[name='size']:checked").val();
-            //let meat = $("input:checkbox[name='meat']:checked").val();
-            //let cheese = $("input:checkbox[name='cheese']:checked").val();
-            //let toppings = $("input:checkbox[name='cheese']:checked").val();
-            var meatUser = [];
-            $.each($("input:checkbox[name='meat']:checked"),
-                function() {
-                    meatUser.push($(this).val());
-
-                });
-            var cheeseUser = [];
-            $.each($("input:checkbox[name='cheese']:checked"),
-                function() {
-                    cheeseUser.push($(this).val());
-
-                });
-            var toppingsUser = [];
-            $.each($("input:checkbox[name='toppings']:checked"),
-                function() {
-                    toppingsUser.push($(this).val());
-
-                });
 
 
 
 
-            //obj for customer's pizza choice
-            let selectedPizza = new Pizza(size, meat, cheese, toppings);
 
-            //final cost of pizza(s)
+//User Interface
+$(document).ready(function() {
+    $("form#pizza-form").submit(function(event) {
+        event.preventDefault();
+        let size = $("input:radio[name='size']:checked").val();
+        //let meat = $("input:checkbox[name='meat']:checked").val();
+        //let cheese = $("input:checkbox[name='cheese']:checked").val();
+        //let toppings = $("input:checkbox[name='cheese']:checked").val();
+        var meatUser = [];
+        $.each($("input:checkbox[name='meat']:checked"),
+            function() {
+                meatUser.push($(this).val());
+
+            });
+        var cheeseUser = [];
+        $.each($("input:checkbox[name='cheese']:checked"),
+            function() {
+                cheeseUser.push($(this).val());
+
+            });
+        var toppingsUser = [];
+        $.each($("input:checkbox[name='toppings']:checked"),
+            function() {
+                toppingsUser.push($(this).val());
+
+            });
+
+        //obj for customer's pizza choice
+        let selectedPizza = new Pizza(size, meatUser, cheeseUser, toppingsUser);
+        console.log(selectedPizza);
+        //final cost of pizza(s)
+        selectedPizza.calculatePizzaCost();
 
 
-            console.log(selectedPizza);
-            //let showCost = selectedPizza.cost;
 
-
-            $("#finalCost").show();
-            $("#showCost").show();
-            $("#finalCost").text(showCost);
-            //console.log(showCost);
-        });
+        $("#showCost").text(selectedPizza.cost);
+        //console.log(showCost);
+        $("#finalCost").show();
     });
+});
